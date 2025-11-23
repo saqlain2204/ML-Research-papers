02cf930a7aa118332a6d6ced2f71f965f2ad3ceb
Mohammed Saqlain <saqlain2204@gmail.com>
2025-11-23 23:19:11 +0530

    fix: add super().init() call in UpSample to resolve nn.Module initialization error

--- UNET/unet_parts.py
+++ UNET/unet_parts.py
@@ -29,6 +29,7 @@ class DownSample(nn.Module):
 
 class UpSample(nn.Module):
     def __init__(self, in_channels, out_channels):
+        super().__init__()
         self.up = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels//2, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels=in_channels, out_channels=out_channels)
     
